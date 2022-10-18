383962cd7a72e0821d03c4b03952fb3b295b27c9
Sergey Kalistratov <rimusolamus@gmail.com>
2022-10-18 11:59:26 +0200

    return json

--- test_app.py
+++ test_app.py
@@ -1,12 +1,14 @@
+import requests
 from flask import Flask
+import json
 
 
 app = Flask(__name__)
 
+url = "https://data.unhcr.org/population/?widget_id=351610&sv_id=54&population_group=5478"
+
 @app.route('/')
 def home():
-	print('\033[1;34m' + 'I can talk!' + '\033[0m')
+	r = json.loads(requests.get(url).content)["data"][0]
+	return r
 
-	return 'Hello. I\'m flask'
-
-	
